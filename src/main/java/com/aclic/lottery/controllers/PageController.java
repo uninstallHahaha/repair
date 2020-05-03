af3c8cfd9565d74aa1166d3bd7946f20fceaf486
uninstallHahaha <2234261505@qq.com>
2020-05-03 22:19:21 +0800

    TODO 使用ws的自动跳转

--- src/main/java/com/aclic/lottery/controllers/PageController.java
+++ src/main/java/com/aclic/lottery/controllers/PageController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
 
-    @RequestMapping("/gettopaypage/{rid}")
+    @RequestMapping("/gettopaypage/{rid}")//
     public String gettopaypage(@PathVariable String rid, Model model){
         model.addAttribute("rid",rid);
         return "check_pay";
